d1c35876974b73af98b15e90ead1b1a8caa7298c
Ayesha Yege <43933845+Ayege@users.noreply.github.com>
2022-04-20 22:52:53 -0400

    First Commit

---
+++ Scripts/SQLQuery10.sql
@@ -0,0 +1,10 @@
+
+
+INSERT INTO BITACORA(Origen,CantidadRegistro,Fecha,Hora,Estado) VALUES ('SQL',?,GETDATE(),GETDATE(),'CORRECTO');
+INSERT INTO BITACORA(Origen,CantidadRegistro,Fecha,Hora,Estado) VALUES ('SQL',?,GETDATE(),GETDATE(),'INCORRECTO');
+INSERT INTO BITACORA(Origen,CantidadRegistro,Fecha,Hora,Estado) VALUES ('CSV',?,GETDATE(),GETDATE(),'CORRECTO');
+INSERT INTO BITACORA(Origen,CantidadRegistro,Fecha,Hora,Estado) VALUES ('CSV',?,GETDATE(),GETDATE(),'INCORRECTO');
+INSERT INTO BITACORA(Origen,CantidadRegistro,Fecha,Hora,Estado) VALUES ('TXT',?,GETDATE(),GETDATE(),'CORRECTO');
+INSERT INTO BITACORA(Origen,CantidadRegistro,Fecha,Hora,Estado) VALUES ('TXT',?,GETDATE(),GETDATE(),'INCORRECTO');
+INSERT INTO BITACORA(Origen,CantidadRegistro,Fecha,Hora,Estado) VALUES ('XLX',?,GETDATE(),GETDATE(),'CORRECTO');
+INSERT INTO BITACORA(Origen,CantidadRegistro,Fecha,Hora,Estado) VALUES ('XLX',?,GETDATE(),GETDATE(),'INCORRECTO');
